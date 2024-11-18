0c963aa56b5e2c9cac9cc57d0a0c55058cb1e5ea
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-11-18 16:08:04 +0000

    Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header id="header">
+      <img
+        src="investment-calculator-logo.png"
+        alt="Green money bag with gold coins stacked next to it."
+      />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
